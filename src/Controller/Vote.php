dc4292b43d6a8ec111fcb6143de2b5835f76966b
Clement <clement@garrot.org>
2018-08-26 13:41:08 +0200

    sauvegarde d'une participation

--- src/Controller/Vote.php
+++ src/Controller/Vote.php
@@ -9,6 +9,7 @@ namespace App\Controller;
 
 
 use App\Entity\Choice;
+use App\Entity\Participation;
 use App\Entity\Proposal;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -16,26 +17,74 @@ class Vote extends Controller
 {
     public function form($url_key){
 
-        $dataForm=[];
-        $dataForm["url_key"]=$url_key;
 
-        $repository = $this->getDoctrine()->getRepository(Proposal::class);
-        $proposal = $repository->findOneBy(['url_key' => $url_key]);
+
+
+        $isSaved=false;
+
+        $redirectSave=$_GET["redirect-save"] ?? "";
+
+        if($redirectSave!=""){
+            $isSaved=true;
+        }
+
+        $dataForm=[];
+        $dataForm["redirect"]=false;
+        $dataForm["url_key"]=$url_key;
+        $dataForm["author"] = $_POST["author"] ?? "";
+
+        $repositoryProposal = $this->getDoctrine()->getRepository(Proposal::class);
+        $proposal = $repositoryProposal->findOneBy(['url_key' => $url_key]);
 
         //si une proposition trouvée
         if(!is_null($proposal)){
             //si données envoyéé en post
             if(count($_POST)) {
-                $error = 0;
-                $dataForm["author"] = $_POST["author"] ?? "";
+                $isSaved=true;
+
+                //TODO : vérification d'erreurs possibles ?
+                $error=0;
+
+                $entityManager = $this->getDoctrine()->getManager();
+
+
                 $dataForm["proposal_id"] = $proposal->getId();
+
+                $participation= new Participation();
+                $participation->setProposal($proposal);
+                $participation->setAuthor($dataForm["author"]);
+                $date_start=new \DateTime();
+                $date_start->setTimestamp(strtotime("now"));
+                $participation->setDateCreate($date_start);
+                $entityManager->persist($participation);
+                $entityManager->flush();
+
+                foreach($proposal->getChoices() as $choice){
+                    $vote_value=$_POST["choice_value_".$choice->getId()] ?? "vote_0";
+                    $vote= new \App\Entity\Vote();
+                    $vote->setProposal($proposal);
+                    $vote->setParticipation($participation);
+                    $vote->setChoice($choice);
+                    $vote->setVoteValue($vote_value);
+                    $entityManager->persist($vote);
+                    $entityManager->flush();
+                }
+
+                $dataForm["redirect"]=true;
+
             }
         }
 
 
 
         $dataForm["proposal"]=$proposal;
-        return $this->render('vote/form.html.twig', $dataForm);
+
+        if($isSaved){
+            return $this->render('vote/save.html.twig', $dataForm);
+        }else{
+            return $this->render('vote/form.html.twig', $dataForm);
+        }
+
 
     }
 }
--- src/Entity/Participation.php
+++ src/Entity/Participation.php
@@ -132,4 +132,5 @@ class Participation
 
         return $this;
     }
+
 }
