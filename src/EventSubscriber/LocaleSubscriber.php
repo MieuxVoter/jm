450a4cd25d4695093c2b134310b37654f6157477
clement <clement@garrot.org>
2019-03-07 19:50:56 +0100

    detection de la langue sur le nom de domaine

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -19,14 +19,21 @@ class LocaleSubscriber implements EventSubscriberInterface
     {
 
         $request = $event->getRequest();
+        $host=$event->getRequest()->getHost();
         $localeParam=$request->query->get('_locale');
         $localeSession=$request->getSession()->get("_locale");
 
         if(is_null($localeSession)){
             $localSession=$this->defaultLocale;
-            $localeBrowser=substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+            /*$localeBrowser=substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
             if($localeBrowser==="fr" || $localeBrowser==="en"){
                 $localSession=$localeBrowser;
+            }*/
+            $host=$event->getRequest()->getHost();
+            if(strstr($host,"jugementmajoritaire")){
+                $localSession="fr";
+            }else{
+                $localSession="en";
             }
             $request->getSession()->set('_locale', $localSession);
         }
